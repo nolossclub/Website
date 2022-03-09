5a0c7153936daed60334fc9229f01c907d042bc1
No Loss Club <95616694+nolossclub@users.noreply.github.com>
2022-03-09 19:25:15 +0530

    Update Land.vue

--- src/components/Land.vue
+++ src/components/Land.vue
@@ -4,7 +4,7 @@
 			<div class="flex md:flex-row flex-col items-center">
 				<div class="md:w-1/2 p-5">
 					<p class="text-3xl font-bold uppercase text-gray-600">NOLOSSCLUB is a crypto-powered savings protocol based on Defi lending. Save money and have a chance to win every week.</p>
-					<div class="py-5">
+					<div class="py-5 hidden md:block">
 						<div class="stats shadow">
 							<div class="stat place-items-center">
 								<div class="stat-value">TVL</div>
